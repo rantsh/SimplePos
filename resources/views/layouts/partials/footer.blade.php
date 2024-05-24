19472ac2c564aa115856c6269ce2821de76e8a83
Roderick Smith <rantsh@gmail.com>
2024-05-23 21:45:35 -0400

    Initial adjustments towards v1

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>{{ __('common.Version') }}</b> 3.0.3
+        <b>{{ __('common.Version') }}</b> 3.0.3
     </div>
-    <strong>{{ __('common.Copyright') }}Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> {{ __('common.all_reserved') }}
-  </footer>
+    <strong style="font-size:6px">{{ __('common.Copyright') }}Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <span style="font-size:6px">{{ __('common.all_reserved') }}</span>
+</footer>
